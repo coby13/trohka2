5af5de416a0a03628ed968fcafd86c1f250ae00e
Elias Tsironis <eliastsirwnhs@gmail.com>
2013-10-08 12:33:23 +0300

    admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131002184905) do
+ActiveRecord::Schema.define(:version => 20131008093008) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(:version => 20131002184905) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => ""
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -100,10 +100,11 @@ ActiveRecord::Schema.define(:version => 20131002184905) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
